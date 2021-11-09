e79e9e0f62aa4d0a8766e6251347eee72d9df0da
Harold Gomez <xagobo14@gmail.com>
2021-11-08 19:14:22 -0500

    primero pasos

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(children: _crearItems()),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = [];
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista
+        ..add(tempWidget)
+        ..add(Divider());
+    }
+    return lista;
+  }
+}
